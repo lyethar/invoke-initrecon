504f07ced99c0e59625de7873c79cc247b019df5
lyethar <107083757+lyethar@users.noreply.github.com>
2023-03-21 14:53:53 -0400

    Update invoke-initrecon.py

--- invoke-initrecon.py
+++ invoke-initrecon.py
@@ -40,7 +40,30 @@ def downloadtooling(tools):
 def invokescan(scope):
 	os.system('cd enumeration')
 	print("Executing Round 1 scans")
-	os.system("nmap -sS -Pn --top-ports 20 -iL " + scope + """ | awk '/^Nmap scan report/{ip=$NF} /open/{print ip >> "open_ports.txt"}'""")
+	output_file = "open_ports.txt"
+	scope_file = scope
+	if os.path.exists(output_file):
+		os.remove(output_file)
+	with open(scope_file, "r") as file:
+		subnets = [line.strip() for line in file if line.strip()]
+	
+	for subnet in subnets:
+		print(f"Scanning subnet {subnet}...")
+		result = subprocess.check_output(["nmap", "-Pn", "-sS", "--top-ports", "20", "--open", subnet], text=True)
+		
+		if "open" in results:
+			print(f"Open ports found in subnet {subnet}:")
+			ips_with_open_ports = [line.split()[4] for line in result.splitlines() if "Nmap scan report for" in line]
+			with open(output_file, "a") as file:
+				for ip in ips_with_open_ports:
+					print(f"IP: {ip}")
+					file.write(ip + "\n")
+			print(f"Saved to {output_file}")
+		else:
+			print(f"No open ports found in subnet {subnet}")
+	print("Done with Round 1 scans")
+			        
+	#os.system("nmap -sS -Pn --top-ports 20 -iL " + scope + """ | awk '/^Nmap scan report/{ip=$NF} /open/{print ip >> "open_ports.txt"}'""")
 	print ("Done with Round 1 ✅")
 	print("Executing Round 2 scans")
 	os.system("sort open_ports.txt | uniq > output_file")
